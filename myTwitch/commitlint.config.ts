414fd199b6285739f2ed74219f1652b5bb824ec6
CristhianQuiñonez <crisrobin-95@hotmail.com>
2025-04-02 16:30:44 +0200

    invalid message

--- myTwitch/commitlint.config.ts
+++
@@ -1,6 +0,0 @@
-import { commitLintFront } from '@alebat/default-alebat-config'
-
-export default commitLintFront
-import { commitLintFront } from '@alebat/default-alebat-config'
-
-export default commitLintFront
--- myTwitch/nuxt.config.ts
+++ myTwitch/nuxt.config.ts
@@ -1,58 +1,15 @@
-import i18nConfig from './config/i18n'
-import { alias } from './config/alias'
-import { cssImports } from './config/css'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  modules: [
-    '@pinia/nuxt',
-    '@nuxtjs/i18n',
-    '@nuxt/image',
-    '@nuxt/test-utils/module',
-    '@nuxtjs/sitemap',
-    '@nuxtjs/robots',
-    '@nuxt/icon',
-    '@nuxtjs/strapi',
-  ],
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: false,
-  spaLoadingTemplate: false,
-  css: cssImports,
-  imports: {
-    dirs: ['core/globals'],
-  },
-  alias,
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@use "sass:map"; @import "assets/styles/mixin.scss";',
-        },
-      },
-    },
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: '_nuxt/[name].[ext]',
-          entryFileNames: '_nuxt/[name].js',
-          chunkFileNames: '_nuxt/[name].js',
-        },
-      },
-    },
-  },
-  i18n: {
-    ...i18nConfig,
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: 'i18n_redirected',
-      redirectOn: 'root',
-      fallbackLocale: 'es',
-    },
-  },
-  strapi: {
-    url: process.env.STRAPI_URL,
-    prefix: '/api',
-    version: 'v5',
-  },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/test-utils'
+  ]
 })
