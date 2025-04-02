e754b45eda041f0464304ead89ce741b62693dd7
CristhianQuiñonez <crisrobin-95@hotmail.com>
2025-04-02 16:55:52 +0200

    asdfsadf

--- myTwitch/eslint.config.js
+++ myTwitch/eslint.config.js
@@ -13,9 +13,9 @@ export default defineConfig([
   pluginVue.configs["flat/essential"],
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
   {
-    "*.{js,vue,ts,json,css,scss,md}": [
-      "eslint --fix",
-      "prettier --write"
-    ]
-  }
+    files: ['pages/*/.vue'],
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
 ]);
